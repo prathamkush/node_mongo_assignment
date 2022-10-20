8aa24618e65d282439d0ebbea43bc35079ab66dd
prathamkush <Pratham.Kushwah@blueyonder.com>
2022-10-20 13:06:58 +0530

    done both collections

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-// first import express, mongoose module
+// first import express, mongoose and routes
 const express = require("express")
 const mongoose = require("mongoose")
+
+// get the routes for learner and courses
+const routes_learner = require("./routes/LearnersRoutes")
+const routes_course = require("./routes/CoursesRoutes")
 
 // initialize express app
 const app = express()
@@ -10,15 +14,23 @@ app.listen(3000, () => {
     console.log("Server running on port 3000")
 })
 
+app.use(express.json())
+
 // default route
 app.use("/welcome", (req,res) => {
     res.send("I am Groot")
 })
 
+// add the routes
+app.use("/learners", routes_learner)
+app.use("/courses", routes_course)
+
+
+
 
 
 // connecting to database
-const uri = "mongodb+srv://pratham520:451228@cluster0.pe7oqtx.mongodb.net/employeeNodejs?retryWrites=true&w=majority"
+const uri = "mongodb+srv://pratham520:451228@cluster0.pe7oqtx.mongodb.net/nodeAssignment1?retryWrites=true&w=majority"
 mongoose.connect(uri, {useNewUrlParser:true}).
         then(() => {console.log("Database Connected")}).
         catch((error) => {console.log(error)})
